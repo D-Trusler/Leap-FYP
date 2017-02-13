b393d66cc75e1bb13dc5b6b68282a52ac9f820fb
Your Name <you@example.com>
2017-02-13 16:25:03 +0000

    adding files

---
+++ fyp/HelloWorld.java
@@ -0,0 +1,30 @@
+package fyp;
+
+import com.leapmotion.leap.*;
+import com.leapmotion.leap.Controller;
+import java.io.*;
+
+
+class HelloWorld{
+	
+
+	public static void main(String[] args) {
+		SampleListener listener = new SampleListener();
+		Controller controller = new Controller();
+		
+		controller.addListener(listener);
+
+		
+		  System.out.println("Press Enter to quit...");
+	        try {
+	            System.in.read();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+
+		
+		controller.removeListener(listener);
+
+	}
+
+}
