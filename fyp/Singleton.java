477ca61e6919383ef2657881b12b8614ca9bb437
user1 <user1@csltp066.ds.leeds.ac.uk>
2017-03-20 16:15:46 +0000

    s

---
+++ fyp/Singleton.java
@@ -0,0 +1,62 @@
+package fyp;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class Singleton {
+	
+	private static Singleton instance = null;
+	
+	private Singleton(){ }
+	
+	private LinkedList<String> commands = new LinkedList<String>();
+	
+	
+	public static Singleton getInstance(){
+		
+		if(instance == null){
+			
+			instance = new Singleton();
+			
+		}
+		
+		synchronized(Singleton.class){
+			if(instance == null){
+				
+				instance = new Singleton();
+				
+			}
+		}
+		
+		return instance;
+	}
+	
+	//reads the first command from our list of commands	
+	public String  read(){
+		
+		if (commands.size() != 0){		
+			String commandtosend = new String();
+			commandtosend = instance.commands.remove(0);
+//			System.out.println("sent: " + commandtosend);
+			return commandtosend;
+		} else {
+			return null;
+		}
+		
+		
+	}
+	
+	public void test(){
+		System.out.println("Singleton Test");
+	}
+	
+	//writes command to end of list
+	public void write(String commandtowrite){
+		if (instance.commands.contains(commandtowrite) == false){
+			instance.commands.add(commandtowrite);}
+//		System.out.println("written: " + commandtowrite);
+
+	}
+
+}
